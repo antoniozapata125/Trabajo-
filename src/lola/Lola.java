e2578e1b98906495a4fa65b339eb95fe87c66d9f
Antonio <eazapatab@alumno.uspg.edu.gt>
2020-08-16 15:53:56 -0600

    hay pan

--- src/lola/Lola.java
+++ src/lola/Lola.java
@@ -17,6 +17,7 @@ public class Lola {
     public static void main(String[] args) {
         System.out.println("hola");
         System.out.println("segundo mensaje");
+        System.out.println("buenas hay pan");
     }
     
 }
