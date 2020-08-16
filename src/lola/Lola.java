9bcba42bc54762413deda76ea97b76d6e10e39d8
Antonio <eazapatab@alumno.uspg.edu.gt>
2020-08-16 15:31:37 -0600

    primer mensaje

---
+++ src/lola/Lola.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lola;
+
+/**
+ *
+ * @author Emmanuel Zapata
+ */
+public class Lola {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola");
+    }
+    
+}
